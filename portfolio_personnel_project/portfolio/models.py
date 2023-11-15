24245752048c906f3e0920a6e72b50d3824e0310
gabrielQ12 <gabriel.quinette1@gmail.com>
2023-11-15 13:52:43 +0100

    Bdd:DONE

--- portfolio_personnel_project/portfolio/models.py
+++ portfolio_personnel_project/portfolio/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Projet(models.Model):
     titre = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    image=models.ImageField(upload_to='portfolio/images/')
-    url=models.URLField(blank=True)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
--- portfolio_personnel_project/portfolio/views.py
+++ portfolio_personnel_project/portfolio/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from.models import Projet
 
-# Create your views here.
+
+def home(request):
+    projets = Projet.objects.all()
+    return render(request,'portfolio/home.html', {'projets':projets})
