fe3812ea21893647d355eed17cd0964d85058edf
gabrielQ12 <gabriel.quinette1@gmail.com>
2023-11-16 12:02:18 +0100

    blog:DONE

--- portfolio_personnel_project/blog/views.py
+++ portfolio_personnel_project/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def mon_blog(request):
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog/mon_blog.html', {'blogs':blogs})
